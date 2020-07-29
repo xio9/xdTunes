797b82bbdafc175cae8dd058905a36d34786112d
Kirill <efremenkov1999@gmail.com>
2020-07-30 02:02:54 +0300

    add: audioPlayer

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,20 @@
 export const radioPlayerInit = () => {
-    console.log("RadioInit");
-}
+    const radio = document.querySelector('.radio');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioHeader = document.querySelector('.radio-header');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;
+
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        audio.src = target.dataset.radioStantion;
+
+        audio.play();
+    });
+};
