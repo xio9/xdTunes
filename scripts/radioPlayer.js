5c7f0e43f674673b12064f05f553a800b00fdbca
Kirill <efremenkov1999@gmail.com>
2020-07-30 13:53:59 +0300

    add: change icons in radio player

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,4 +1,5 @@
 export const radioPlayerInit = () => {
+
     const radio = document.querySelector('.radio');
     const radioCoverImg = document.querySelector('.radio-cover__img');
     const radioHeader = document.querySelector('.radio-header');
@@ -11,20 +12,35 @@ export const radioPlayerInit = () => {
 
     radioStop.disabled = true;
 
+    // Смена иконки на stop и play 
+    const changeIconPlay = () => {
+        if (audio.paused) {
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else{
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+        }
+    };
+
+    // Функция для подключения радио 
     radioNavigation.addEventListener('change', event => {
         const target = event.target;
         radioStop.disabled = false;
         
         audio.src = target.dataset.radioStantion;
-
+        
         audio.play();
+        changeIconPlay();
     });
 
+    // Функция для проигрыша радио
     radioStop.addEventListener('click', () =>{
         if (audio.paused){
             audio.play();
         }else{
             audio.pause();
         }
+        changeIconPlay();
     });
 };
