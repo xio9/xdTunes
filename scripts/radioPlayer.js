ad196edf7db00c980e5081c6db1952b2a405d3d6
Kirill <efremenkov1999@gmail.com>
2020-07-30 14:12:52 +0300

    add: radio name in header

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -2,7 +2,7 @@ export const radioPlayerInit = () => {
 
     const radio = document.querySelector('.radio');
     const radioCoverImg = document.querySelector('.radio-cover__img');
-    const radioHeader = document.querySelector('.radio-header');
+    const radioHeaderBig = document.querySelector('.radio-header__big');
     const radioNavigation = document.querySelector('.radio-navigation');
     const radioItem = document.querySelectorAll('.radio-item');
     const radioStop = document.querySelector('.radio-stop');
@@ -15,19 +15,31 @@ export const radioPlayerInit = () => {
     // Смена иконки на stop и play 
     const changeIconPlay = () => {
         if (audio.paused) {
+            radio.classList.remove('play'); //Иконка крутится при запуске радио)))
             radioStop.classList.add('fa-play');
             radioStop.classList.remove('fa-stop');
         } else{
+            radio.classList.add('play');  //Иконка перестает крутиться при стопе радио
             radioStop.classList.add('fa-stop');
             radioStop.classList.remove('fa-play');
         }
     };
 
+    // Серая рамка выбранного радио
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select');
+    }
     // Функция для подключения радио 
     radioNavigation.addEventListener('change', event => {
         const target = event.target;
+        const parrent = target.closest('.radio-item');
+        selectItem(parrent);
+
+        const title = parrent.querySelector('.radio-name').textContent;
+        radioHeaderBig.textContent = title;
+
         radioStop.disabled = false;
-        
         audio.src = target.dataset.radioStantion;
         
         audio.play();
